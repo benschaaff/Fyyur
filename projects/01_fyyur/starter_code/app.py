bea6581c88470575b6ac22692131098c8662946a
Benjamin Schaaff <ben@benjamins-mbp.attlocal.net>
2019-11-10 23:27:58 -0800

    add flask migrate, connect to local db, and set SQLALCHEMY_TRACK_MODIFICATIONS to false; sort imports

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -3,15 +3,20 @@
 #----------------------------------------------------------------------------#
 
 import json
-import dateutil.parser
-import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 import logging
-from logging import Formatter, FileHandler
+from logging import FileHandler, Formatter
+
+from flask import (Flask, Response, flash, redirect, render_template, request,
+                   url_for)
+from flask_sqlalchemy import SQLAlchemy
+
+import babel
+import dateutil.parser
+from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_wtf import Form
 from forms import *
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,8 +25,7 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-# TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
