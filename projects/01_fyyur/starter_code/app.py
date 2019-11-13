2058267b6a5e13ca12633429f5f48bb268b4de91
ben <benschaaff@gmail.com>
2019-11-13 02:18:06 -0800

    create show works now

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -461,15 +461,25 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
+  form = request.form
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  show = Show(artist_id=form['artist_id'],
+                  venue_id=form['venue_id'],
+                  start_time=form['start_time'])
+
+  #TODO(ben): maybe refactor this
+  try:
+    db.session.add(show)
+    db.session.commit()
+    message = f'Show: artist {show.artist_id} at venue {show.venue_id} was successfully listed!', 'info'
+  except:
+    db.session.rollback()
+    message = f'An error occurred. Show: artist {show.artist_id} at venue {show.venue_id} could not be listed.', 'danger'
+
+  flash(*message)
+
   return render_template('pages/home.html')
+
 
 @app.errorhandler(404)
 def not_found_error(error):
