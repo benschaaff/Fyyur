ddb6a0d3b8e4f4e956fd841b26965aff6e8c82f4
ben <benschaaff@gmail.com>
2019-11-15 21:17:57 -0800

    move models and app initialization into separate file models.py

---
+++ models.py
@@ -0,0 +1,124 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_moment import Moment
+from typing import Dict, List
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+# TODO: add repr to venue
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    genres = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    email = db.Column(db.String(120), nullable=False)
+
+    shows = db.relationship('Show', backref='venue',
+                            lazy='dynamic', passive_deletes=True)
+
+    @staticmethod
+    def _format_shows(shows: List['Show']) -> List[Dict]:
+        return [
+            {
+                "artist_id": show.artist.id,
+                "artist_name": show.artist.name,
+                "artist_image_link": show.artist.image_link,
+                "start_time": str(show.start_time)
+            }
+            for show in shows
+        ]
+
+    @property
+    def past_shows(self) -> List[Dict]:
+        shows = self.shows.filter(Show.start_time < datetime.utcnow()).all()
+        return self._format_shows(shows)
+
+    @property
+    def upcoming_shows(self) -> List[Dict]:
+        shows = self.shows.filter(Show.start_time >= datetime.utcnow()).all()
+        return self._format_shows(shows)
+
+
+# TODO: add repr to artist
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String)
+    website = db.Column(db.String(120))
+    email = db.Column(db.String(120), nullable=False)
+
+    shows = db.relationship('Show', backref='artist', lazy='dynamic',
+                            cascade='all, delete-orphan', passive_deletes=True)
+
+    @staticmethod
+    def _format_shows(shows: List['Show']) -> List[Dict]:
+        return [
+            {
+                "venue_id": show.venue.id,
+                "venue_name": show.venue.name,
+                "venue_image_link": show.venue.image_link,
+                "start_time": str(show.start_time)
+            }
+            for show in shows
+        ]
+
+    @property
+    def past_shows(self) -> List[Dict]:
+        shows = self.shows.filter(Show.start_time < datetime.utcnow())
+        return self._format_shows(shows)
+
+    @property
+    def upcoming_shows(self) -> List[Dict]:
+        shows = self.shows.filter(Show.start_time >= datetime.utcnow())
+        return self._format_shows(shows)
+
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'Artist.id', ondelete='CASCADE'))
+    venue_id = db.Column(db.Integer, db.ForeignKey(
+        'Venue.id', ondelete='CASCADE'))
+    start_time = db.Column(db.DateTime)
+
+    #TODO: learn to do multiline f string
+    def __repr__(self):
+        return (
+            f'<Show {self.id} \nVenue: {self.venue.name} \nArtist: {self.artist.name} \nTime: {self.start_time}>\n'
+        )
+
+db.create_all()
